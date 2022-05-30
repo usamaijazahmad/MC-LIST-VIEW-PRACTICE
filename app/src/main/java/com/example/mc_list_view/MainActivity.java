e4ba663440233b24a3afa305be4573b8aaa44115
Usama Ijaz <usamaijaz2001@gmail.com>
2022-05-30 20:33:33 +0500

    Added java code.

--- app/src/main/java/com/example/mc_list_view/MainActivity.java
+++ app/src/main/java/com/example/mc_list_view/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.mc_list_view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView lv;
+    ArrayList<String> studentlist= new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv=findViewById(R.id.listview);
+
+        studentlist.add("Usama");
+        studentlist.add("Maaz");
+        studentlist.add("Wahab");
+        studentlist.add("Mateeh");
+        studentlist.add("Danyal");
+        studentlist.add("Umer");
+        studentlist.add("Qasim");
+        studentlist.add("Faizan");
+
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,studentlist);
+        lv.setAdapter(arrayAdapter);
+
     }
 }
